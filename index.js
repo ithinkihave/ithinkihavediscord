21890e00f866aeb9b0176a1b6545c7b144a9cb43
Parmjot Singh <parmjotsinghrobot@gmail.com>
2026-03-25 14:41:09 +1300

    feat(truefalse): separate links into separate files

--- index.js
+++ index.js
@@ -4,23 +4,8 @@ const { Client, GatewayIntentBits, messageLink } = require("discord.js");
 const ithinkihaveserver = "1435477855596318742";
 const 中文 = "1486174868054474762";
 
-const trueresponses = [
-  "https://tenor.com/view/true-love-gif-18324778560370268269",
-  "https://tenor.com/view/fentcord-true-coyote-gif-2599436844608872721",
-  "true!",
-  "https://tenor.com/view/morgan-freeman-true-seal-gif-8993121496866626214",
-  "https://tenor.com/view/memes-gif-9980668056796018353",
-  "https://tenor.com/qroSoIP9tfK.gif",
-  "https://tenor.com/view/whatever-go-my-scarab-whatever-go-my-true-scarab-go-my-scarab-morgan-freeman-gif-3091971228562826566",
-  "https://tenor.com/view/he-made-a-statement-so-good-even-his-gang-praised-him-dog-check-mark-happy-green-gif-8429843223482741276",
-  "https://tenor.com/view/he-made-a-statement-so-factual-even-his-gang-applauded-him-gif-7484528142807708083",
-];
-const falseresponses = [
-  "false",
-  "https://tenor.com/view/fact-check-kellanrockssoccer-gif-2569170256995872124",
-  "https://tenor.com/view/he-made-such-a-bad-he-made-he-made-such-a-bad-statement-gif-8431545385226321029",
-  "https://tenor.com/view/opps-gif-16378882966061421087",
-];
+const trueresponses = require("./res/true.json");
+const falseresponses = require("./res/false.json");
 
 const client = new Client({
   intents: [
