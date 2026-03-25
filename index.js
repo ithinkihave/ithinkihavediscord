775a48f75282a072ffce663077d2fcaa8a4303ff
Parmjot Singh <parmjotsinghrobot@gmail.com>
2026-03-26 12:57:14 +1300

    feat: delete messages if they do not contain enough chinese... ...stickers are exempt

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const { Client, GatewayIntentBits } = require("discord.js");
 
 const ITHINKIHAVE_SERVER_ID = "1435477855596318742";
 const CHINESE_CHANNEL_ID = "1486174868054474762";
-const ENGLISH_REGEX = /[a-zA-Z]/;
+const ALLOWED_CHINESE_CHANNEL_CHAR_REGEX =
+  /[\p{sc=Han} \n\r\u3000.,!?。，、！？…]/u;
+const ALLOWED_CHINESE_CHANNEL_MENTION_REGEX = /<@!?\d+>|<@&\d+>/gu;
+const HAN_CHARACTER_REGEX = /\p{sc=Han}/u;
+const CHINESE_CHARACTER_RATIO_THRESHOLD = 0.5;
+const ASCII_ART_MIN_NON_WHITESPACE_LENGTH = 24;
+const ASCII_ART_REPEATED_CHARACTER_MIN_COUNT = 10;
+const ASCII_ART_REPEATED_CHARACTER_RATIO_THRESHOLD = 0.35;
 const TRUTH_CHECK_PHRASES = ["is this true", "这是真的吗", "is this real"];
 const SERVER_NAME_PREFIXES = ["i think", "我觉得", "我想"];
 
@@ -17,7 +24,6 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessageTyping,
-    GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessageReactions,
   ],
 });
@@ -65,7 +71,7 @@ async function handleMessage(message, eventType) {
 
   logMessage(message, eventType);
 
-  if (await handleChineseChannelEnglishCheck(message)) {
+  if (await handleChineseChannelContentCheck(message)) {
     return;
   }
 
@@ -108,15 +114,90 @@ function logMessage(message, eventType) {
   }
 }
 
-function shouldDeleteEnglishInChineseChat(message) {
-  return (
-    message.channel?.id === CHINESE_CHANNEL_ID &&
-    ENGLISH_REGEX.test(message.content ?? "")
-  );
+function shouldDeleteDisallowedChineseChannelMessage(message) {
+  if (message.channel?.id !== CHINESE_CHANNEL_ID) {
+    return false;
+  }
+
+  if (message.attachments.size > 0) {
+    return true;
+  }
+
+  if (message.stickers.size > 0 && !(message.content ?? "").trim()) {
+    return false;
+  }
+
+  return shouldDeleteChineseChannelContent(message.content ?? "");
 }
 
-async function handleChineseChannelEnglishCheck(message) {
-  if (!shouldDeleteEnglishInChineseChat(message)) {
+function shouldDeleteChineseChannelContent(text) {
+  const textWithoutMentions = text.replace(ALLOWED_CHINESE_CHANNEL_MENTION_REGEX, "");
+  let chineseCharacterCount = 0;
+  let countableCharacterCount = 0;
+
+  for (const symbol of textWithoutMentions) {
+    if (!ALLOWED_CHINESE_CHANNEL_CHAR_REGEX.test(symbol)) {
+      return true;
+    }
+
+    if (isNeutralChineseChannelFormatting(symbol)) {
+      continue;
+    }
+
+    countableCharacterCount += 1;
+
+    if (HAN_CHARACTER_REGEX.test(symbol)) {
+      chineseCharacterCount += 1;
+    }
+  }
+
+  if (countableCharacterCount === 0) {
+    return true;
+  }
+
+  if ((chineseCharacterCount / countableCharacterCount) < CHINESE_CHARACTER_RATIO_THRESHOLD) {
+    return true;
+  }
+
+  return isSuspiciousRepeatedCharacterArt(textWithoutMentions);
+}
+
+function isNeutralChineseChannelFormatting(symbol) {
+  return symbol === " " || symbol === "\n" || symbol === "\r" || symbol === "\u3000";
+}
+
+function isSuspiciousRepeatedCharacterArt(text) {
+  const characterCounts = new Map();
+  let nonWhitespaceCharacterCount = 0;
+
+  for (const symbol of text) {
+    if (isNeutralChineseChannelFormatting(symbol)) {
+      continue;
+    }
+
+    nonWhitespaceCharacterCount += 1;
+    characterCounts.set(symbol, (characterCounts.get(symbol) ?? 0) + 1);
+  }
+
+  if (nonWhitespaceCharacterCount < ASCII_ART_MIN_NON_WHITESPACE_LENGTH) {
+    return false;
+  }
+
+  let maxRepeatedCharacterCount = 0;
+
+  for (const count of characterCounts.values()) {
+    if (count > maxRepeatedCharacterCount) {
+      maxRepeatedCharacterCount = count;
+    }
+  }
+
+  return maxRepeatedCharacterCount >= ASCII_ART_REPEATED_CHARACTER_MIN_COUNT &&
+    (maxRepeatedCharacterCount / nonWhitespaceCharacterCount) >=
+      ASCII_ART_REPEATED_CHARACTER_RATIO_THRESHOLD;
+}
+
+async function handleChineseChannelContentCheck(message) {
+  if (!shouldDeleteDisallowedChineseChannelMessage(message)) {
     return false;
   }
 
