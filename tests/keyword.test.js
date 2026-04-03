a84b3d831dd0a4e9696f2f3572348a8757f7c59a
Nathan <47441022+NathanSnail@users.noreply.github.com>
2026-04-04 12:52:39 +1300

    test: keyword tests

---
+++ tests/keyword.test.js
@@ -0,0 +1,19 @@
+import { describe, it } from "node:test";
+import { getResponses } from "../lib/keywordCheck.js";
+import { deepStrictEqual } from "node:assert";
+
+describe("Guh", () => {
+  it("Should say guh", () => {
+    deepStrictEqual(getResponses("i say guh"), [
+      "https://tenor.com/view/guh-gif-25116077",
+    ]);
+  });
+  it("Should say guh once", () => {
+    deepStrictEqual(getResponses("guh guh guh guh guh"), [
+      "https://tenor.com/view/guh-gif-25116077",
+    ]);
+  });
+  it("Should only say guh when i guh", () => {
+    deepStrictEqual(getResponses("... glup"), []);
+  });
+});
