e18db17351d9e073ad62982442baf5c3e0cd7de8
Nathan <47441022+NathanSnail@users.noreply.github.com>
2026-04-04 15:34:49 +1300

    tests: fix copillot

--- tests/chinese.test.js
+++ tests/chinese.test.js
@@ -1,28 +1,26 @@
 import assert from "node:assert/strict";
 import { describe, it } from "node:test";
-import { shouldDeleteDisallowedChineseChannelMessage } from "../lib/chineseCheck.js";
+import { shouldDeleteChineseChannelContent } from "../lib/chineseCheck.js";
 
 describe("No english", () => {
   it("Shouldn't allow english chars", () => {
-    assert(shouldDeleteDisallowedChineseChannelMessage("Hello World"));
-    assert(shouldDeleteDisallowedChineseChannelMessage("A"));
+    assert(shouldDeleteChineseChannelContent("Hello World"));
+    assert(shouldDeleteChineseChannelContent("A"));
   });
   it("Shouldn't let you use emojis to break the rules", () => {
-    assert(
-      shouldDeleteDisallowedChineseChannelMessage(":regional_indicator_A:"),
-    );
-    assert(shouldDeleteDisallowedChineseChannelMessage("🇪"));
+    assert(shouldDeleteChineseChannelContent(":regional_indicator_A:"));
+    assert(shouldDeleteChineseChannelContent("🇪"));
   });
   it("Should allow pings", () => {
-    assert(!shouldDeleteDisallowedChineseChannelMessage("<@1234>"));
-    assert(!shouldDeleteDisallowedChineseChannelMessage("<@&1234>"));
+    assert(!shouldDeleteChineseChannelContent("<@1234> 一"));
+    assert(!shouldDeleteChineseChannelContent("<@&1234> 一"));
   });
 });
 
 describe("Yes chinese", () => {
   it("Should allow punctuation", () => {
     assert(
-      !shouldDeleteDisallowedChineseChannelMessage(
+      !shouldDeleteChineseChannelContent(
         `朝辞白帝彩云间，
 千里江陵一日还。
 两岸猿声啼不住，
@@ -32,9 +30,7 @@ describe("Yes chinese", () => {
   });
   it("Should work with pings", () => {
     assert(
-      !shouldDeleteDisallowedChineseChannelMessage(
-        "<@495780356302045195> 生日快乐",
-      ),
+      !shouldDeleteChineseChannelContent("<@495780356302045195> 生日快乐"),
     );
   });
 });
@@ -42,7 +38,7 @@ describe("Yes chinese", () => {
 describe("No art", () => {
   it("Should block sus lines", () => {
     assert(
-      shouldDeleteDisallowedChineseChannelMessage(`
+      shouldDeleteChineseChannelContent(`
 . . ... .   .   ...
 ... ..  .   .   . .
 . . ... ... ... ...
@@ -51,7 +47,7 @@ describe("No art", () => {
   });
   it("Shouldn't allow painting with chinese chars", () => {
     assert(
-      shouldDeleteDisallowedChineseChannelMessage(`
+      shouldDeleteChineseChannelContent(`
 日间日间日日日间日间间间日间间间日日日
 日日日间日日间间日间间间日间间间日间日
 日间日间日日日间日日日间日日日间日日日
