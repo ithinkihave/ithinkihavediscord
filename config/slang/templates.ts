8fa36ed4e2ae5d47b09e8abcc3e49bd4804e2397
Nathan <47441022+NathanSnail@users.noreply.github.com>
2026-04-15 20:42:35 +1200

    fix(slang): remove useless graves

--- config/slang/templates.ts
+++ config/slang/templates.ts
@@ -1,7 +1,7 @@
 export const templates = [
 	`# DID YOU KNOW!
 **$short** is short for **$long**!`,
-	`erm... did you mean *$short*`,
+	"erm... did you mean *$short*",
 	"**FYI**: __$short__ is easier to say",
 	"You could've said $short.",
 	"William Lee would never say $long, he would say $short",
